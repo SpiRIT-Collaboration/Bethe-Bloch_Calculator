196fa4e6493daae30e9078d4b5c88f396c2f30e2
mkaneko4869 <m.kaneko4869@gmail.com>
2019-03-14 10:31:45 +0900

    testRootFinder.C Modified to work with current MassEstimator.h Modified to define RootFinder class outside of the loop.

--- mk_NewFitter_20190111/testRootFinder.C
+++ mk_NewFitter_20190111/testRootFinder.C
@@ -4,11 +4,13 @@ using namespace MassEstimator;
 void testRootFinder()
 {
 	// parameters for the eq. function.
-	Double_t *p = new Double_t[6];
+	Double_t *p = new Double_t[8];
 	p[0] = 1.;
 	p[1] = 0.;
-	p[2] = 12.;
-	p[3] = 1.;
+	p[2] = 1.;
+	p[3] = 0.;
+	p[4] = 12.;
+	p[5] = 1.;
 
 	auto out = new TFile("test.root","recreate");
 	auto tree = new TTree("tree","");
@@ -20,7 +22,13 @@ void testRootFinder()
 	tree->Branch("mass",&mass,"mass/D");
 	tree->Branch("calMass",calMass,"calMass[7]/D");
 
+	auto finder = new ROOT::Math::RootFinder;
 	Double_t dx = 0.01;  // minute displacement for the derivation.
+	auto func  = [p](double x){ return LVMassFinderEq(&x,p); };
+	auto dfunc = [p,dx](double x){ return LVMassFinderDeriv(&x,p,dx);};
+	auto func1d  = new ROOT::Math::Functor1D(func);
+	auto gradf1d = new ROOT::Math::GradFunctor1D(func,dfunc);
+
 	for(Int_t i=0; i<50000; i++){
 
 		mom = mass = dedx = -1.;
@@ -30,21 +38,15 @@ void testRootFinder()
 		while(mass<=0) mass = 938.7*gRandom->Gaus(1.,0.2);
 		dedx = fLVdedx(1.,mass,&mom,p);
 
-		p[4] = mom;
-		p[5] = dedx;
-		auto func  = [p](double x){ return LVMassFinderEq(&x,p); };
-		auto dfunc = [p,dx](double x){ return LVMassFinderDeriv(&x,p,dx);};
+		p[6] = mom;
+		p[7] = dedx;
 
-		ROOT::Math::Functor1D func1d(func);
-		ROOT::Math::GradFunctor1D gradf1d(func,dfunc);
-
-		ROOT::Math::RootFinder finder;
 
 		// kBRENT
-		finder.SetMethod(ROOT::Math::RootFinder::kBRENT);
-		finder.SetFunction(func1d,0.1,10000.);
-		finder.Solve();
-		calMass[0] = finder.Root();
+		finder->SetMethod(ROOT::Math::RootFinder::kBRENT);
+		finder->SetFunction(*func1d,0.1,10000.);
+		finder->Solve();
+		calMass[0] = finder->Root();
 
 		// These three make a lot of error messages (when calculation failed).
 		// If you want to turn off the error or info. messages, set global variable like below.
@@ -52,41 +54,41 @@ void testRootFinder()
 		/*
 		// BISECTION
 		finder.SetMethod(ROOT::Math::RootFinder::kGSL_BISECTION);
-		finder.SetFunction(func1d,0.1,10000.);
+		finder.SetFunction(*func1d,0.1,10000.);
 		finder.Solve();
 		calMass[1] = finder.Root();
 
 		// FALSE_POS
 		finder.SetMethod(ROOT::Math::RootFinder::kGSL_FALSE_POS);
-		finder.SetFunction(func1d,0.1,10000.);
+		finder.SetFunction(*func1d,0.1,10000.);
 		finder.Solve();
 		calMass[2] = finder.Root();
 
 		// GSL_BRENT
 		finder.SetMethod(ROOT::Math::RootFinder::kGSL_BRENT);
-		finder.SetFunction(func1d,0.1,10000.);
+		finder.SetFunction(*func1d,0.1,10000.);
 		finder.Solve();
 		calMass[3] = finder.Root();
 		*/
 
 		// need derivative !
 		// Newton
-		finder.SetMethod(ROOT::Math::RootFinder::kGSL_NEWTON);
-		finder.SetFunction(gradf1d,1000.);
-		finder.Solve();
-		calMass[4] = finder.Root();
+		finder->SetMethod(ROOT::Math::RootFinder::kGSL_NEWTON);
+		finder->SetFunction(*gradf1d,1000.);
+		finder->Solve();
+		calMass[4] = finder->Root();
 
 		// Secant
-		finder.SetMethod(ROOT::Math::RootFinder::kGSL_SECANT);
-		finder.SetFunction(gradf1d,1000.);
-		finder.Solve();
-		calMass[5] = finder.Root();
+		finder->SetMethod(ROOT::Math::RootFinder::kGSL_SECANT);
+		finder->SetFunction(*gradf1d,1000.);
+		finder->Solve();
+		calMass[5] = finder->Root();
 
 		// Steffenson
-		finder.SetMethod(ROOT::Math::RootFinder::kGSL_STEFFENSON);
-		finder.SetFunction(gradf1d,1000.);
-		finder.Solve();
-		calMass[6] = finder.Root();
+		finder->SetMethod(ROOT::Math::RootFinder::kGSL_STEFFENSON);
+		finder->SetFunction(*gradf1d,1000.);
+		finder->Solve();
+		calMass[6] = finder->Root();
 
 
 		tree->Fill();
